e5b6687b928c911373bd5c09ca9e816b1b2263ae
Eugene Stolbin <estolbin@gmail.com>
2022-03-25 20:54:05 +0300

    Homework DONE

--- IntroductionToOOP/DataContainers/DataContainers/BinaryTree/main.cpp
+++ IntroductionToOOP/DataContainers/DataContainers/BinaryTree/main.cpp
@@ -32,10 +32,21 @@ public:
 	{
 		cout << "TConstructor:\t" << this << endl;
 	}
+	Tree(const std::initializer_list<int>& il) : Tree()
+	{
+		for (int const* it = il.begin(); it != il.end(); it++)
+			insert(*it);
+	}
+	Tree(const Tree& other) : Tree()
+	{
+		*this = other;
+	}
 	~Tree()
 	{
+		clearAll(Root);
 		cout << "TDestructor:\t" << this << endl;
 	}
+
 
 	void insert(int Data)
 	{
@@ -66,7 +77,62 @@ public:
 		print(Root);
 		cout << endl;
 	}
+
+	void erase(int Data)
+	{
+		erase(Data, Root);
+	}
+
 private:
+
+
+	Element* erase(int Data, Element* Root)
+	{
+		if (Root == nullptr) return Root;
+		if (Data < Root->Data) Root->pLeft = erase(Data, Root->pLeft);
+		else if (Data > Root->Data) Root->pRight = erase(Data, Root->pRight);
+		else
+		{
+			//             нет потомков (детей)
+			if (Root->pLeft == nullptr && Root->pRight == nullptr) return nullptr;
+			else if (Root->pLeft == nullptr)
+			{
+				Element* temp = Root->pRight;
+				delete Root;
+				return temp;
+			}
+			else if (Root->pRight == nullptr)
+			{
+				Element* temp = Root->pLeft;
+				delete Root;
+				return temp;
+			}
+
+			Element* temp = minValueNode(Root->pRight); 
+			Root->Data = temp->Data;
+			Root->pRight = erase(temp->Data, Root->pRight);
+		}
+		return Root;
+	}
+
+	Element* minValueNode(Element* Root)
+	{
+		Element* Temp = Root;
+		while (Temp && Temp->pLeft != nullptr) Temp = Temp->pLeft;
+
+		return Temp;
+	}
+
+	void clearAll(Element* Root)
+	{
+		if (Root)
+		{
+			clearAll(Root->pLeft);
+			clearAll(Root->pRight);
+			delete Root;
+		}
+	}
+
 	void insert(int Data, Element* Root)
 	{
 		if (this->Root == nullptr)this->Root = new Element(Data);
@@ -155,15 +221,22 @@ void main()
 	cout << "Количество элементов в дереве: " << tree.count() << endl;
 	cout << "Сумма элементов дерева: " << tree.sum() << endl;
 	cout << "Среднее арифметическое: " << tree.avg() << endl;
-	UniqueeTree uniq_tree;
-	for (int i = 0;i < n;i++)
-	{
-		uniq_tree.insert(rand() % 100);
-	}
-	uniq_tree.print();
-	//cout << endl;
-	cout << "Минимальное значение в дереве: " << uniq_tree.minValue() << endl;
-	cout << "Максимальное значение в дереве: " << uniq_tree.maxValue() << endl;
-	cout << "Количество элементов в дереве: " << uniq_tree.count() << endl;
+	//UniqueeTree uniq_tree;
+	//for (int i = 0;i < n;i++)
+	//{
+	//	uniq_tree.insert(rand() % 100);
+	//}
+	//uniq_tree.print();
+	////cout << endl;
+	//cout << "Минимальное значение в дереве: " << uniq_tree.minValue() << endl;
+	//cout << "Максимальное значение в дереве: " << uniq_tree.maxValue() << endl;
+	//cout << "Количество элементов в дереве: " << uniq_tree.count() << endl;
+	//cout << "Сумма элементов дерева: " << uniq_tree.sum() << endl;
+	//cout << "Среднее арифметическое: " << uniq_tree.avg() << endl;
+
+	int i;
+	cout << "Введите удаляемое значение: "; cin >> i;
+	tree.erase(i);
+	tree.print();
 
 }
